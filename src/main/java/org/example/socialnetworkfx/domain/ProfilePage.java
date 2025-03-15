f47d111e1dbec1313501cb0161399501ff985185
bxpxi <155725673+bxpxi@users.noreply.github.com>
2025-03-15 20:51:04 +0200

    Add files via upload

---
+++ src/main/java/org/example/socialnetworkfx/domain/ProfilePage.java
@@ -0,0 +1,17 @@
+package org.example.socialnetworkfx.socialnetworkfx.domain;
+
+public class ProfilePage {
+    private String fullName;
+
+    public ProfilePage(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
